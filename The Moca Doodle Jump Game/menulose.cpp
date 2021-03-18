179d02c7c49bf35f9d6d73c4ee4f5d260d78a678
Panthonf <76035497+Panthonf@users.noreply.github.com>
2021-03-18 15:29:26 +0700

    new updated

---
+++ The Moca Doodle Jump Game/menulose.cpp
@@ -0,0 +1,55 @@
+#include "menulose.h"
+
+menulose::menulose(float width, float height)
+{
+	if (!font.loadFromFile("font/f1.ttf"))
+	{
+
+	}
+
+	this->menu[1].setFont(font);
+	this->menu[1].setCharacterSize(80);
+	this->menu[1].setFillColor(sf::Color::Yellow);
+	this->menu[1].setString("PLAY AGAIN");
+	this->menu[1].setOrigin(menu[1].getLocalBounds().width / 2, this->menu[1].getLocalBounds().height / 2);
+	this->menu[1].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEM + 1) * 2));
+
+	
+
+	selectecItem = 0;
+}
+
+menulose ::~menulose()
+{
+
+}
+
+void menulose::draw(sf::RenderWindow& window)
+{
+	for (int i = 0; i < MAX_ITEM; i++)
+	{
+		window.draw(menu[i]);
+	}
+}
+
+void menulose::MoveUp()
+{
+	if (selectecItem - 1 >= 0)
+	{
+		this->menu[selectecItem].setFillColor(sf::Color::Yellow);
+		selectecItem--;
+		this->menu[selectecItem].setFillColor(sf::Color::Red);
+
+	}
+}
+
+void menulose::MoveDown()
+{
+	if (selectecItem + 1 < MAX_ITEM)
+	{
+		this->menu[selectecItem].setFillColor(sf::Color::Yellow);
+		selectecItem++;
+		this->menu[selectecItem].setFillColor(sf::Color::Red);
+
+	}
+}
